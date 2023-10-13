78aa59aea324bac8e3f3dde46780fc8e69d54c09
LoshanRSN <rasanloshan46@gmail.com>
2023-10-13 17:10:55 +0200

    ajout d'addition et soustraction - Loshan

--- Calculatrice_RuRa/src/Calculatrice/Calculatrice.java
+++ Calculatrice_RuRa/src/Calculatrice/Calculatrice.java
@@ -5,4 +5,24 @@ import java.util.Stack;
 class Calculatrice {
 	Stack <Double> accumulateur = new Stack<>();
 	Stack <Double> operande = new Stack<>();
+	
+	public void addition () {
+		if (accumulateur.empty()) {
+			accumulateur.push(operande.pop()+operande.pop());
+		}
+		else {
+			accumulateur.push(accumulateur.peek()+operande.pop());
+		}
+	}
+	
+	public void soustraction () {
+		if (accumulateur.empty()) {
+			accumulateur.push(-operande.pop()+operande.pop());
+		}
+		else {
+			accumulateur.push(-accumulateur.peek()+operande.pop());
+		}
+	}
 }
+
+
