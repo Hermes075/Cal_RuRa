6dd0c004c1ed516f71c48c7e67915d9544aa0c8e
arthu <arthur.rubio@etu.mines-ales.fr>
2023-10-13 17:17:22 +0200

    ajout multiplication - Arthur

--- Calculatrice_RuRa/src/Calculatrice/Calculatrice.java
+++ Calculatrice_RuRa/src/Calculatrice/Calculatrice.java
@@ -5,4 +5,13 @@ import java.util.Stack;
 class Calculatrice {
 	Stack <Double> accumulateur = new Stack<>();
 	Stack <Double> operande = new Stack<>();
+	
+	public void multiplication (){
+		if (accumulateur.empty()){
+			accumulateur.push(operande.pop()*operande.pop());
+		}
+		else {
+			accumulateur.push(accumulateur.peek()*operande.pop());
+	}
+}
 }
