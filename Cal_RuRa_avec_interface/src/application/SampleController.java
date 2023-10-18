373b6b733e84d11bfbab22a0af2d0bc79da6cbcf
arthu <arthur.rubio@etu.mines-ales.fr>
2023-10-18 15:16:29 +0200

    Import des attributs main

--- Cal_RuRa_avec_interface/src/application/SampleController.java
+++ Cal_RuRa_avec_interface/src/application/SampleController.java
@@ -1,5 +1,13 @@
 package application;
 
+import application.Main;
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 public class SampleController {
-	
+	private void handleButtonClick(int buttonIndex) {
+		if ()
+        
+        }
+    }
 }
