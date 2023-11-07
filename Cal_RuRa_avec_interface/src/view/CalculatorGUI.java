e6c10e11ad4f6887f04b6aac9422cd470d732a59
LoshanRSN <rasanloshan46@gmail.com>
2023-11-07 08:50:26 +0100

    correction - Loshan

--- Cal_RuRa_avec_interface/src/view/CalculatorGUI.java
+++ Cal_RuRa_avec_interface/src/view/CalculatorGUI.java
@@ -84,7 +84,7 @@ public class CalculatorGUI extends Scene implements CalculatorGUIInterface {
             "0", "1", "2", "3", "4",
             "5", "6", "7", "8","9",
             "POP", ".", "SWAP", "+/-","+",
-            "-", "x", "/", "RR","AC"
+            "-", "x", "/", "RR	","AC"
         };
 
         for (int i = 0; i < buttons.length; i++) {
