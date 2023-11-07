543dc26bf479137bf47acdaad60f923c3c19c442
LoshanRSN <rasanloshan46@gmail.com>
2023-11-07 14:06:51 +0100

    correction suite - Loshan

--- Cal_RuRa_avec_interface/src/controler/CalculatorControlerInterface.java
+++ Cal_RuRa_avec_interface/src/controler/CalculatorControlerInterface.java
@@ -1,7 +1,5 @@
 package controler;
 
-import java.util.List;
-
 public interface CalculatorControlerInterface {
 	//public String change();
 	//public List<Double> change();
--- Cal_RuRa_avec_interface/src/model/CalculatorModelInterface.java
+++ Cal_RuRa_avec_interface/src/model/CalculatorModelInterface.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.Stack;
-
 public interface CalculatorModelInterface {
 	
 	public void add();
